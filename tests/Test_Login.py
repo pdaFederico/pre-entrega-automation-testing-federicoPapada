5ac051621df1a093bcf0085efbf8b09042bab631
Federico <pdafederico@gmail.com>
2025-10-24 20:11:34 -0300

    Se agrega tiempo de espera explicita y validación de /inventory.html

--- tests/Test_Login.py
+++ tests/Test_Login.py
@@ -4,17 +4,20 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
-from utils.helpers import get_driver, login_test
+from utils.helpers import get_driver, login_test, wait_time
 
 @pytest.fixture
 def driver():
     driver = get_driver()
     yield driver
+    print("\nMantener ventana abierta para revisión...")
+    time.sleep(5)
     driver.quit()
 
 #Ingreso al sitio
 
 def test_login(driver):
     login_test(driver)
-    assert "inventory" in driver.current_url
-    time.sleep(5)
+    assert "/inventory.html" in driver.current_url
+    wait_time(driver)
+
