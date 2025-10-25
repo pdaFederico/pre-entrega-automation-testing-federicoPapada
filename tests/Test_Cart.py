51aca084dfe34efb074ebe958276934bde772b19
Federico <pdafederico@gmail.com>
2025-10-25 02:43:08 -0300

    Creación de TC para automatizar flujo de compras y validar cantidad de productos añadidos al carrito

---
+++ tests/Test_Cart.py
@@ -0,0 +1,29 @@
+import time
+import pytest
+import sys
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+
+from utils.helpers import get_driver, login_test, wait_time, product_search
+
+@pytest.fixture
+def driver():
+    driver = get_driver()
+    yield driver
+    print("\nMantener ventana abierta para revisión...")
+    time.sleep(5)
+    driver.quit()
+
+def test_cart(driver):
+    login_test(driver)
+    wait_time(driver)
+    product_search(driver)
+    badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge')
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, "shopping_cart_badge"), "1"))
+    badge = badge.text
+    assert badge == "1"
+    print(f"Test OK: Productos en el carrito: {badge}")
