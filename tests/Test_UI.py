c1a18e8895e3fe1674f0dfdddb266abaa066ddd4
Federico <pdafederico@gmail.com>
2025-10-25 01:06:53 -0300

    Test Case completo incluyendo validación de titulos, verificación de inventario y presencia de elementos criticos en la interfaz

--- tests/Test_UI.py
+++ tests/Test_UI.py
@@ -21,5 +21,16 @@ def test_interface(driver):
     assert "/inventory.html" in driver.current_url
     expected_title = "Swag Labs"
     assert driver.title == expected_title, f"Título de página incorrecto. Esperado: {expected_title}, Actual: {driver.title}"
-    products = driver.find_elements(By.CLASS_NAME, 'inventory_list')
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
     assert len(products)> 0
+    first_product = products [0]
+    product_name = first_product.find_element(By.CLASS_NAME, 'inventory_item_name')
+    product_name = product_name.text
+    print(f"    Nombre del Producto: {product_name}")
+    product_price = first_product.find_element(By.CLASS_NAME, 'inventory_item_price')
+    product_price = product_price.text
+    print(f"    Precio del producto: {product_price}")
+    buttonMenu = driver.find_element(By.ID, 'react-burger-menu-btn')
+    cartLink = driver.find_element(By.CLASS_NAME, 'shopping_cart_link')
+    searchFilter = driver.find_element(By.CLASS_NAME, 'product_sort_container') 
+    
