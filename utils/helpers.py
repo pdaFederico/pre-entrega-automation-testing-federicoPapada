b8a7b87165baa9411800962425bee0fb2c871b3d
pdaFederico <pdafederico@gmail.com>
2025-12-11 21:29:53 -0300

    Modificaciones en el archivo helpers de fixtures que no se utilizaran

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,29 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-URL = 'https://www.saucedemo.com/'
-USERNAME = 'visual_user'
-PASSWORD = 'secret_sauce'
-
-
 def get_driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     return driver
 
-def wait_time(driver):
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "shopping_cart_link")))
 
-
-def login_test(driver):
-    driver.get(URL)     #Ingresa a la URL
-    driver.find_element(By.ID, "user-name").send_keys(USERNAME)
-    driver.find_element(By.ID, "password").send_keys(PASSWORD)
-    driver.find_element(By.ID, "login-button").click()
-
+    
